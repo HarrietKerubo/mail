3dc7a15cd414fe5f5c199717111e80a85370cbb2
Harriet Kerubo <keruboharriet7@gmail.com>
2023-08-28 08:24:57 -0400

    showing email content

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -18,6 +18,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector("#emails-view").style.display = "none";
   document.querySelector("#compose-view").style.display = "block";
+  document.querySelector("#display-view").style.display = "none";
   let recipients = document.querySelector("#compose-recipients");
   let subject = document.querySelector("#compose-subject");
   let body = document.querySelector("#compose-body");
@@ -35,6 +36,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector("#emails-view").style.display = "block";
   document.querySelector("#compose-view").style.display = "none";
+  document.querySelector("#display-view").style.display = "none";
 
   // Show the mailbox name
   document.querySelector("#emails-view").innerHTML = `<h3>${
@@ -64,7 +66,14 @@ function load_mailbox(mailbox) {
 
         const emailClass = isRead ? "read-email" : "unread-email";
 
-        mails += `<div class="mail-border ${emailClass}" type="button" data-emailID ="${emailID}"> 
+        mails += `<div class="mail-border ${emailClass}" type="button" 
+        data-emailID ="${emailID}"
+        data-recipient = "${emailRecipient}"
+        data-sender = "${emailSender}"
+        data-subject = "${emailSubject}"
+        data-body = "${emailBody}"
+        data-timestamp = "${emailTimestamp}"
+        > 
         <li>
         <span class="sender">${emailSender} </span>
         <span class="subject">${emailSubject} </span>
@@ -81,7 +90,13 @@ function load_mailbox(mailbox) {
       allMails.forEach((item) => {
         item.addEventListener("click", () => {
           emailID = item.getAttribute("data-emailID");
-          view_email(emailID);
+          recipient = item.getAttribute("data-recipient");
+          sender = item.getAttribute("data-sender");
+          subject = item.getAttribute("data-subject");
+          body = item.getAttribute("data-body");
+          timestamp = item.getAttribute("data-timestamp");
+
+          view_email(emailID, recipient, sender, subject, body, timestamp);
         });
       });
     });
@@ -111,7 +126,40 @@ function send_mail(recipients, subject, body) {
 }
 
 // function view_email(id, sender, recipient, subject, body, timestamp) {
-function view_email(id) {
-  console.log(id);
+function view_email(id, recipient, sender, subject, body, timestamp) {
+  // Show the email display and hide other views
+  document.querySelector("#display-view").style.display = "block";
+  document.querySelector("#emails-view").style.display = "none";
+  document.querySelector("#compose-view").style.display = "none";
+
+  fetch(`/emails/${id}`)
+    .then((response) => response.json())
+    // pass the attributes that you got from function load_mail and then declare an anonymous function () to execute required code
+    .then(
+      (id,
+      recipient,
+      sender,
+      subject,
+      timestamp,
+      () => {
+        document.querySelector("#display-view").innerHTML = `
+        <div id="meta-data">
+        <div> 
+        <span class="title"> From: </span>
+        <span class="content"> ${sender} </span> </div>
+        <div>  <span class="title"> To:  </span>
+        <span class="content"> ${recipient} </span></div>
+        <div> <span class="title"> Subject: </span>
+        <span class="content"> ${subject} </span> </div>
+        <div>  <span class="title"> Timestamp: </span>
+        <span class="content"> ${timestamp} </span></div>
+        </div>
+        <div>
+        <input id="reply-btn" type="button" class="btn btn-primary" value="Reply" />
+        </div>
+        <hr />
+        <div style="font-size: 20px"> ${body} </div>
+        `;
+      })
+    );
 }
-const mailInstance = document.querySelectorAll(".mail-border");
